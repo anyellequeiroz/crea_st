0307da69141cc4383b177c7ee1274f3f159819bd
anyelle queiroz <anyelle.queiroz@ee.ufcg.edu.br>
2025-05-31 16:13:00 -0300

    design

--- app.py
+++ app.py
@@ -9,39 +9,88 @@ cols = st.columns([1, 3, 1])
 with cols[1]:
     selected_option = st.radio("", ["Overview", "Mapeamento de Obras", "Relatório automático",  "Rotas de Fiscalização", "About"], horizontal=True)
 
-# Main content based on selection
+st.markdown(
+    """
+    <style>
+    [data-testid="stAppViewContainer"] {
+        background-image: url("https://images.unsplash.com/photo-1614850523011-8f49ffc73908?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 if selected_option == "Overview":
 
-    st.title("🧠 CreIA: Inteligência que constrói, visão que previne")
+    #st.title("🧠 CreIA: Inteligência que constrói, visão que previne")
+    st.markdown('<h1 style="color: white;">🧠 CreIA: Inteligência que constrói, visão que previne</h1>', unsafe_allow_html=True)
+
+    # texto = """
+    # CreIA é um sistema de inteligência artificial que utiliza redes neurais convolucionais para identificar imagens de obras e obter seus endereços. Com visão computacional, auxilia na geração de rotas de fiscalização e criação de relatórios.
+    # Uma ferramenta inovadora para inspeções eficientes por parte do CREA-PB.
+    # """
+
+    # st.markdown(
+    #     f"""
+    #     <div style='text-align: justify; font-size: 16px;'>
+    #         {texto}
+    #     """,
+    #     unsafe_allow_html=True
+    # )
 
     texto = """
-    CreIA é um sistema de inteligência artificial que utiliza redes neurais convolucionais para identificar imagens de obras e diagnosticar problemas estruturais. Com visão computacional, auxilia na detecção precoce de falhas em construções.
-    Uma ferramenta inovadora para inspeções mais seguras e eficientes na engenharia civil.
+    CreIA é um sistema de inteligência artificial que utiliza redes neurais convolucionais para identificar imagens de obras e obter seus endereços. Com visão computacional, auxilia na geração de rotas de fiscalização e criação de relatórios.
+    Uma ferramenta inovadora para inspeções eficientes por parte do CREA-PB.
     """
 
     st.markdown(
         f"""
-        <div style='text-align: justify; font-size: 16px;'>
+        <div style='text-align: justify; font-size: 16px; color: white;'>
             {texto}
+
         """,
         unsafe_allow_html=True
     )
 
-    # logo = Image.open("logo.png")
-    # st.image(logo, width=150)
+    logo = Image.open("logo.png")
+    crea_logo = Image.open("crea-logo.png")
+    hack_logo = Image.open("hack-logo.png")
 
-    # st.markdown(
-    # """
-    # <div style='text-align: center;'>
-    #     <img src='logo.png' width='150'>
-    # </div>
-    # """,
-    # unsafe_allow_html=True
-    # )
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+
+    col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+
+    with col1:
+        st.write("")
+
+    with col2:
+        st.write("")
+
+    with col3:
+        st.image(logo, width=150)
+
+    with col4:
+        st.image(hack_logo, width=150)
+
+    with col5:
+        st.image(crea_logo, width=150)
+
+    with col6:
+        st.write("")
+
+    with col7:
+        st.write("")
 
 elif selected_option == "Mapeamento de Obras":
     st.subheader("Sistema inteligente para monitoramento urbano por meio da análise de imagens aéreas e identificação de obras em andamento 🛰️")
-
     uploaded_file = st.file_uploader("📤 Carregar um arquivo:")
 
     if uploaded_file is not None:
@@ -176,7 +225,6 @@ elif selected_option == "Rotas de Fiscalização":
 
         else:
             st.write('ok')
-
 
 elif selected_option == "About":
     st.title("About")
